c6c3e764e6064bbeee83c9134bf41b5cb4aec025
Julien Duchesne <julien.duchesne@grafana.com>
2021-11-02 21:23:16 -0400

    Add full language support in the VS Code plugin Adds (and improves) the language configuration from https://github.com/liamdawson/vscode-jsonnet-language (which took it from https://github.com/heptio/vscode-jsonnet) This makes this vscode plugin fully support the jsonnet language without any dependencies
    I want to make this releasable in vscode ASAP so I can start testing iteratively (in real cases, not a development instance). With formatting and the navigation it currently has, it's already much better than the alternatives
    
    Next steps:
    1. Make the vscode extension download the latest language server release automatically
    1. Release the jsonnet language server in a Github Actions upon tagging
    1. Release and publish the vscode plugin in a Github Actions upon tagging
    
    Signed-off-by: Julien Duchesne <julien.duchesne@grafana.com>

--- editor/vscode-client/src/extension.ts
+++ editor/vscode-client/src/extension.ts
@@ -47,8 +47,8 @@ export function activate(context: ExtensionContext) {
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
+		'jsonnetLanguageServer',
+		'Jsonnet Language Server',
 		serverOptions,
 		clientOptions
 	);
