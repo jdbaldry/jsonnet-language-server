62cb432fa747deedd198be6d93fb43d39baf29e7
Julien Duchesne <julien.duchesne@grafana.com>
2021-11-22 21:55:25 -0500

    Use the `flag` standard lib
    Signed-off-by: Julien Duchesne <julien.duchesne@grafana.com>

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -38,19 +38,18 @@ var (
 )
 
 // printVersion prints version text to the provided writer.
-func printVersion(w io.Writer) {
+func printVersion() {
+	w := flag.CommandLine.Output()
 	fmt.Fprintf(w, "%s version %s\n", name, version)
 }
 
 // printVersion prints help text to the provided writer.
-func printHelp(w io.Writer) {
-	printVersion(w)
+func printHelp() {
+	w := flag.CommandLine.Output()
+
+	printVersion()
 	fmt.Fprintln(w)
-	fmt.Fprintf(w, "Options:\n")
-	fmt.Fprintf(w, "  -h / --help        Print this help message.\n")
-	fmt.Fprintf(w, "  -J / --jpath <dir> Specify an additional library search dir\n")
-	fmt.Fprintf(w, "                     (right-most wins).\n")
-	fmt.Fprintf(w, "  -v / --version     Print version.\n")
+	flag.PrintDefaults()
 	fmt.Fprintln(w)
 	fmt.Fprintf(w, "Environment variables:\n")
 	fmt.Fprintf(w, "  JSONNET_PATH is a %q separated list of directories\n", filepath.ListSeparator)
@@ -64,24 +63,17 @@ func printHelp(w io.Writer) {
 func main() {
 	jpaths := filepath.SplitList(os.Getenv("JSONNET_PATH"))
 
-	for i, arg := range os.Args {
-		if arg == "-h" || arg == "--help" {
-			printHelp(os.Stdout)
-			os.Exit(0)
-		} else if arg == "-v" || arg == "--version" {
-			printVersion(os.Stdout)
-			os.Exit(0)
-		} else if arg == "-J" || arg == "--jpath" {
-			if i == len(os.Args)-1 {
-				fmt.Printf("Expected value for option %s but found none.\n", arg)
-				fmt.Println()
-				printHelp(os.Stdout)
-				os.Exit(1)
-			}
+	flag.Usage = printHelp
+	versionArg := flag.Bool("version", false, "Print version and exit")
+	jpathArg := flag.String("jpath", "", "Specify an additional library search dir (right-most wins)")
+	flag.Parse()
 
-			jpaths = append([]string{os.Args[i+1]}, jpaths...)
-		}
+	if *versionArg {
+		printVersion()
+		os.Exit(0)
 	}
+
+	jpaths = append([]string{*jpathArg}, jpaths...)
 
 	log.Println("Starting the language server")
 
